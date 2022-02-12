fdb92d3db8dde5a0c67879da68d244df47b3a46c
FunXzpro <98162737+FunXzpro@users.noreply.github.com>
2022-02-12 12:59:04 +0200

    an cool update

--- inputprogram.js
+++ inputprogram.js
@@ -1,32 +1,32 @@
 const changeMainImage = (itemName) => {
-    const objImages = {
-      youtube: 'images/ya ya 1.png' ,
-      discord: 'images/ya ya 5.png' ,
-      instagram: 'images/ya ya 3.png' ,
-      github: 'images/ya ya 4.png' ,
-      twitter: 'images/ya ya 2.png' ,
-      default: 'images/ya ya.png' 
-    };
-    const img = document.getElementById('main-photo')
-    if (itemName && objImages[itemName]) {
-      img.src = objImages[itemName];
-    } else {
-      img.src = objImages.default;
-    }
+  const objImages = {
+    youtube: 'ya_ya_youtube.png' ,
+    discord: 'ya_ya_discord.png' ,
+    instagram: 'ya_ya_instagram.png' ,
+    github: 'ya_ya_github.png' ,
+    twitter: 'ya_ya_twitter.png' ,
+    default: 'ya_ya_default.png' 
   };
+  const img = document.getElementById('main-photo');
+  let usedImage = objImages.default;
+  if (itemName && objImages[itemName]) {
+    usedImage = objImages[itemName];
+  }
+  img.src = `/images/${usedImage}`;
+};
   
-  
-  window.addEventListener('DOMContentLoaded', (event) => {
-      changeMainImage('default');
-      document.querySelectorAll("a[data-item]").forEach((item) => {
-        console.log(item);
-        const itemType = item.getAttribute('data-item');
-  
-        item.addEventListener('mouseover',()=>{
-          changeMainImage(itemType);
-        });
-        item.addEventListener('mouseout',()=>{
-          changeMainImage('default');
-        });
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    changeMainImage('default');
+    document.querySelectorAll("a[data-item]").forEach((item) => {
+      console.log(item);
+      const itemType = item.getAttribute('data-item');
+
+      item.addEventListener('mouseover',() => {
+        changeMainImage(itemType);
       });
-  });
+      item.addEventListener('mouseout',() => {
+        changeMainImage('default');
+      });
+    });
+});
